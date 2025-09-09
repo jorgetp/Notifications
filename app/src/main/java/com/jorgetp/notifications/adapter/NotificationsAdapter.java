9755b014bce4f96166c00647d33d6a55409c2d67
Jorge Toro <43438910+jorgetp@users.noreply.github.com>
2025-09-09 04:18:57 +0200

    Update item layout text appearance and color
    Set textAppearance and textColor for a TextView in item.xml to improve UI consistency. Minor formatting change in NotificationsAdapter for code style.

--- app/src/main/java/com/jorgetp/notifications/adapter/NotificationsAdapter.java
+++ app/src/main/java/com/jorgetp/notifications/adapter/NotificationsAdapter.java
@@ -67,8 +67,7 @@ public class NotificationsAdapter extends RecyclerView.Adapter<NotificationsAdap
     @NonNull
     @Override
     public NotificationsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item,
-                parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
         return new NotificationsAdapter.ViewHolder(view);
     }
 
